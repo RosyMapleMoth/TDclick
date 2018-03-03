faaa473a8ac4deed8ad8a81eb1f293a0e0e33c55
Seaney F. Shell <afc76927@opayq.com>
2018-03-02 17:40:27 -0800

    added some basic class files to be expanded on

---
+++ TDclickGame/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    public int Money;
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(30, 30, 300, 40), Money.ToString());
+
+    }
+
+
+
+    void Update ()
+    {
+		
+	}
+
+
+
+
+
+}
